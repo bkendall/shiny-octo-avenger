1c92a952253e4e1f78eb4e146388f63847743219
Bryan Kendall <bryan@runnable.com>
2015-07-26 16:38:23 -0700

    removing lab tests for cucumber tests

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -3,15 +3,12 @@ var Client = require('../../').client;
 
 module.exports = function () {
   this.Before(function (callback) {
-    this.priorNodeEnv = process.env.NODE_ENV;
-    process.env.NODE_ENV = 'test';
     this.port = 8080;
     this.client = new Client('localhost:' + this.port);
     this.server = Server.listen(this.port, callback);
   });
 
   this.After(function (callback) {
-    process.env.NODE_ENV = this.priorNodeEnv;
     this.server.close(callback);
   });
 };
