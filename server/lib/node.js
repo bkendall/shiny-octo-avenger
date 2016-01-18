62ecbced4814d268e73f73196961edb0cf024845
Bryan Kendall <bryan@bryankendall.com>
2016-01-18 06:29:13 +0000

    rewrite node with class

--- server/lib/node.js
+++ server/lib/node.js
@@ -6,45 +6,46 @@ var hasProps = require('101/has-properties');
 var boom = require('boom');
 var uuid = require('uuid');
 
-module.exports = Node;
-
 var nodes = [];
 
-function Node (label, value) {
-  assign(this, {
-    id: uuid(),
-    label: label,
-    value: value
-  });
-  nodes.push(this);
+class Node {
+  constructor (label, value) {
+    assign(this, {
+      id: uuid(),
+      label: label,
+      value: value
+    });
+    nodes.push(this);
+  }
+
+  static find (opts, cb) {
+    if (Object.keys(opts).length === 0) {
+      return cb(null, nodes);
+    }
+    var n = nodes.filter(hasProps(opts));
+    if (!n.length) {
+      cb(boom.notFound('node does not exist'));
+    } else {
+      cb(null, n);
+    }
+  }
+
+  static findOne (opts, cb) {
+    this.find(opts, function (err, _nodes) {
+      cb(err, err ? null : _nodes[0]);
+    });
+  }
+
+  delete (cb) {
+    var i = findIndex(nodes, hasProps({ id: this.id }));
+    nodes.splice(i, 1);
+    cb(null);
+  }
+
+  update (opts, cb) {
+    assign(this, opts);
+    cb(null, this);
+  }
 }
 
-Node.findOne = function (opts, cb) {
-  this.find(opts, function (err, _nodes) {
-    cb(err, err ? null : _nodes[0]);
-  });
-};
-
-Node.find = function (opts, cb) {
-  if (Object.keys(opts).length === 0) {
-    return cb(null, nodes);
-  }
-  var n = nodes.filter(hasProps(opts));
-  if (!n.length) {
-    cb(boom.notFound('node does not exist'));
-  } else {
-    cb(null, n);
-  }
-};
-
-Node.prototype.update = function (opts, cb) {
-  assign(this, opts);
-  cb(null, this);
-};
-
-Node.prototype.delete = function (cb) {
-  var i = findIndex(nodes, hasProps({ id: this.id }));
-  nodes.splice(i, 1);
-  cb(null);
-};
-
+module.exports = Node;
