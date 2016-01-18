0e7a10a22620e3b776ef578b061c8f65b81466b9
Bryan Kendall <bryan@bryankendall.com>
2016-01-18 06:32:34 +0000

    rewrite association with node 4 class

--- server/lib/association.js
+++ server/lib/association.js
@@ -6,46 +6,48 @@ var hasProps = require('101/has-properties');
 var isFunction = require('101/is-function');
 var uuid = require('uuid');
 
-module.exports = Association;
-
 var associations = [];
 
-function Association (fromID, label, toID) {
-  assign(this, {
-    id: uuid(),
-    from: fromID,
-    label: label,
-    to: toID
-  });
-  associations.push(this);
+class Association {
+  constructor (fromID, label, toID) {
+    assign(this, {
+      id: uuid(),
+      from: fromID,
+      label: label,
+      to: toID
+    });
+    associations.push(this);
+  }
+
+  static count (fromID, label, cb) {
+    if (isFunction(label)) {
+      cb = label;
+      label = undefined;
+    }
+    var opts = {
+      from: fromID
+    };
+    if (label) {
+      opts.label = label;
+    }
+    var _associations = associations.filter(hasProps(opts));
+    cb(null, _associations.length);
+  }
+
+  static fetch (fromID, label, cb) {
+    var _associations = associations
+      .filter(hasProps({
+        from: fromID,
+        label: label
+      }));
+    cb(null, _associations);
+  }
+
+  delete (cb) {
+    var i = findIndex(associations, hasProps({ id: this.id }));
+    associations.splice(i, 1);
+    cb(null);
+  }
 }
 
-Association.fetch = function (fromID, label, cb) {
-  var _associations = associations
-    .filter(hasProps({
-      from: fromID,
-      label: label
-    }));
-  cb(null, _associations);
-};
-
-Association.count = function (fromID, label, cb) {
-  if (isFunction(label)) {
-    cb = label;
-    label = undefined;
-  }
-  var opts = {
-    from: fromID
-  };
-  if (label) {
-    opts.label = label;
-  }
-  var _associations = associations.filter(hasProps(opts));
-  cb(null, _associations.length);
-};
-
-Association.prototype.delete = function (cb) {
-  var i = findIndex(associations, hasProps({ id: this.id }));
-  associations.splice(i, 1);
-  cb(null);
-};
+module.exports = Association;
